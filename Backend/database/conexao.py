181e66d38ac44ca5dba48be6bcbce8674d9a5bb2
RubensGJ <rubinho7.rr@gmail.com>
2025-05-27 10:28:02 -0400

    feat: Projeto feito

--- Backend/database/conexao.py
+++ Backend/database/conexao.py
@@ -4,6 +4,6 @@ def conecta_bd():
     return psycopg2.connect(
         host='localhost',
         database='projeto',
-        user='postgres',
-        password='Rubinho091123'
+        user='Jester',
+        password='💣✌☺⚐☼👎⚐👍✋☠☟⚐'
     )
